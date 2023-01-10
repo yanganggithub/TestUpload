13374ae9ec36c0697e3bf293d33e5baf80117f3e
kevin <1053316540@qq.com>
2023-01-10 11:32:53 +0800

    local

---
+++ TestUpload/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  TestUpload
+//
+//  Created by ganhai on 1/6/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if window == nil {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.makeKeyAndVisible()
+        }
+        window?.rootViewController = HomeViewController()
+        return true
+    }
+
+   
+
+
+}
+
---
+++ TestUpload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestUpload
+//
+//  Created by ganhai on 1/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
