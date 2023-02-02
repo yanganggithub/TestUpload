12459a7270e62354b356c30fd178c6b1b1dc1aac
kevin <1053316540@qq.com>
2023-02-02 14:19:33 +0800

    m

--- TestUpload/AppDelegate.swift
+++ TestUpload/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window?.makeKeyAndVisible()
         }
         window?.rootViewController = HomeViewController()
+        //
         return true
     }
 
